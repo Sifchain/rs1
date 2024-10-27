0c320dbc906280b1ef3a4263a55d6993b45e7fa1
Monil Patel <monilpat@gmail.com>
2024-10-26 22:21:41 -0700

    revert

--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false,
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+  reactStrictMode: true,
+  swcMinify: true,
   env: {
     MONGODB_URI: process.env.MONGODB_URI,
     OPENAI_API_KEY: process.env.OPENAI_API_KEY,
@@ -15,53 +13,6 @@ const nextConfig = {
         source: '/index',
         destination: '/',
         permanent: true,
-      },
-      {
-        source: '/(.*)',
-        has: [
-          {
-            type: 'header',
-            key: 'x-forwarded-proto',
-            value: 'http',
-          },
-        ],
-        destination: 'https://realityspiral.com/:path*',
-        permanent: true,
-      },
-    ]
-  },
-  async headers() {
-    return [
-      {
-        // Apply security headers globally
-        source: '/(.*)',
-        headers: [
-          {
-            key: 'Strict-Transport-Security',
-            value: 'max-age=63072000; includeSubDomains; preload', // Enforce HTTPS for 2 years
-          },
-          {
-            key: 'Content-Security-Policy',
-            value:
-              "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline'; connect-src 'self'; img-src 'self'; style-src 'self' 'unsafe-inline'; font-src 'self';",
-          },
-          {
-            key: 'X-Frame-Options',
-            value: 'DENY', // Prevent Clickjacking
-          },
-          {
-            key: 'X-Content-Type-Options',
-            value: 'nosniff', // Prevent MIME type sniffing
-          },
-          {
-            key: 'Referrer-Policy',
-            value: 'no-referrer', // Ensure referrer info is not sent
-          },
-          {
-            key: 'Permissions-Policy',
-            value: 'camera=(), microphone=(), geolocation=()', // Restrict permissions
-          },
-        ],
       },
     ]
   },
