5077f5bc56ceaabeaf710b69ea65f250f6534f8a
Monil Patel <monilpat@gmail.com>
2024-11-07 15:15:44 -0800

    Added analytics

--- src/components/TwitterShareButton.js
+++ src/components/TwitterShareButton.js
@@ -1,4 +1,5 @@
 import { Button } from '@chakra-ui/react'
+import { track } from '@vercel/analytics'
 
 const TwitterShareButton = ({ text, url, hashtags }) => {
   // URL-encode parameters for the Twitter URL
@@ -10,10 +11,14 @@ const TwitterShareButton = ({ text, url, hashtags }) => {
   const twitterUrl = `https://twitter.com/intent/tweet?text=${tweetText}&url=${tweetUrl}${
     tweetHashtags ? `&hashtags=${tweetHashtags}` : ''
   }`
-
+  const handleClick = () => {
+    track('Twitter Share Button Clicked', {
+      text,
+    })
+  }
   return (
     <a href={twitterUrl} target="_blank" rel="noopener noreferrer">
-      <Button colorScheme="twitter" variant="solid">
+      <Button colorScheme="twitter" variant="solid" onClick={handleClick}>
         Tweet This!
       </Button>
     </a>
