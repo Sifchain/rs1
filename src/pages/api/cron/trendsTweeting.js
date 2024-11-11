a3d58f963fad6c3b545050700ce01d63e6667630
Monil Patel <monilpat@gmail.com>
2024-11-10 19:58:31 -0800

    Comment out for now

--- src/pages/api/cron/trendsTweeting.js
+++ src/pages/api/cron/trendsTweeting.js
@@ -7,24 +7,24 @@ import { generateImage } from '@/utils/ai'
 export default async function handler(req, res) {
   try {
     // Fetch and process trending topics
-    const trends = await fetchTrendingTopics()
+    // const trends = await fetchTrendingTopics()
 
     // Iterate over each trend, summarize and post response
-    for (const trend of trends) {
-      const { summary, suggestedTweets } = await summarizeTweetsForTrend(trend)
-      // Generate an image related to the tweet content
-      const imagePrompt = `Create a vivid, visually engaging image that represents the essence of the following tweet: "${suggestedTweets[0]}"`
-      const imageUrl = await generateImage(imagePrompt)
-      // Post suggested tweet to Twitter with image generated from summary
-      // TODO: Add authenticated client
-      //   const tweetResponse = await tweetImageWithText(
-      //     twitterClient,
-      //     imageUrl,
-      //     suggestedTweets[0]
-      //   )
-      //   console.log({ tweetResponse })
-      console.log(`Posted response for trend: ${trend}`)
-    }
+    // for (const trend of trends) {
+    //   const { summary, suggestedTweets } = await summarizeTweetsForTrend(trend)
+    // Generate an image related to the tweet content
+    //   const imagePrompt = `Create a vivid, visually engaging image that represents the essence of the following tweet: "${suggestedTweets[0]}"`
+    //   const imageUrl = await generateImage(imagePrompt)
+    // Post suggested tweet to Twitter with image generated from summary
+    // TODO: Add authenticated client
+    //   const tweetResponse = await tweetImageWithText(
+    //     twitterClient,
+    //     imageUrl,
+    //     suggestedTweets[0]
+    //   )
+    //   console.log({ tweetResponse })
+    //   console.log(`Posted response for trend: ${trend}`)
+    // }
 
     res.status(200).json({ message: 'Bot ran successfully.' })
   } catch (error) {
